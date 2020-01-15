61250a49e29e9437cbb7d9f3e43acff294723117
maxtheaxe <max.perrello@gmail.com>
2020-01-14 20:05:35 -0500

    some fixes, still not fully functional

--- helper-app/src/app.js
+++ helper-app/src/app.js
@@ -40,35 +40,46 @@ app.setHandler({
 	},
 
 	CafeIntent() {
-		const stat = getBobsInfo();
-		console.log(stat);
-		if (stat) { // if it's true, tell the user it's open
+		const statResult = getBobsInfo();
+		if (statResult === true) { // if it's true, tell the user it's open
 			this.tell("Bobs is currently open.");
 		}
 		else { // if it's false, tell the user it's closed
 			this.tell("Bobs is currently closed.");
 		}
+		console.log("Result: " + statResult); // testing
 	},
 });
 
 // getBobsInfo() - calls cafebonappetit API v2 and returns current information
 async function getBobsInfo() {
+	const cafeID = '1447'; // set cafe id here, will be easier to adapt for all later
+	const apiBase = 'https://legacy.cafebonappetit.com/api/2/cafes?cafe='; // base api
 	const options = {
-		uri: 'https://legacy.cafebonappetit.com/api/2/cafes?cafe=1447', // api url
+		uri: apiBase.concat(cafeID), // base api url concat w selected cafe's ID
 		json: true // parses json string
 	};
 	const data = await requestPromise(options); // call api and store in data
-	const dayInfo = data.cafes['1447'].days['0'].dayparts; // grab open periods for today
-	return checkOpen(dayInfo); // feeds dayInfo to checkOpen, which returns boolean
+	const dayInfo = data.cafes[cafeID].days['0'].dayparts; // grab open periods for today
+	console.log("dayparts: " + dayInfo); // testing
+	const dayStatus = data.cafes[cafeID].days['0'].status; // grab status for today
+	console.log("Status String: " + dayStatus); // testing
+	return checkOpen(dayInfo, dayStatus); // feeds dayInfo to checkOpen, which returns boolean
 }
 
 // checkOpen(dayInfo) - takes current day info and checks to see if current time is
 // during an open period (must be better way to do this using api, but it's unclear how)
-function checkOpen(dayInfo) { // need to add support for different time zones later
+function checkOpen(dayInfo, dayStatus) { // need to add support for different time zones later
+	// check whether it's closed for the day by comparing status string with "closed"
+	if (dayStatus === "closed") { // if strings match, then
+		return false; // return false, indicating it's currently closed
+	} // if not, just move on
 	var now = new Date(); // grab current date and time
 	var compareStart = new Date(); // grab current date again for easier comparing later
 	var compareEnd = new Date(); // grab current date again for easier comparing later
-	for (dayPart in dayInfo) { // loop over each period of day that cafe is open
+	// loop over each period of day that cafe is open (for length of list at indices)
+	for (var dayPart = 0; dayPart < dayInfo.length; dayPart++) {
+		console.log("each dayPart: " + dayInfo[dayPart]); // testing
 		// setup endtime date object for comparison purposes
 		// split current period endtime string on colon into list
 		var endTime = dayInfo[dayPart].endtime.split(':');
